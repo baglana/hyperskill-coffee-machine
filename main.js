9a217ee4fe825886f50c9bcef373562618b2d2c4
baglana <102351104+baglana@users.noreply.github.com>
2023-08-04 18:01:28 +0600

    3/6: Coffee for all

--- main.js
+++ main.js
@@ -15,32 +15,65 @@ Coffee is ready!`
 );
 */
 
-const readCups = () => {
-  const prompt = "Write how many cups of coffee you will need:\n";
-  return input(prompt);
-};
-
-const calcIngredients = (cups) => {
-  console.log(`For ${cups} cups of coffee you will need:`);
-
-  function Ingredient(name, amount, unit) {
-    this.name = name;
-    this.amount = amount;
-    this.unit = unit;
-  }
-
-  const coffee = [
-    new Ingredient("water", 200, "ml"),
-    new Ingredient("milk", 50, "ml"),
-    new Ingredient("coffee beans", 15, "g")
-  ];
-
-  coffee.forEach((ingredient) => {
-    let line = ingredient.amount * cups + " ";
-    line += ingredient.unit + " of ";
-    line += ingredient.name;
-    console.log(line);
-  })
+function Ingredient(name, amount, unit) {
+  this.name = name;
+  this.amount = amount;
+  this.unit = unit;
 }
 
-calcIngredients(readCups());
+const coffee = [
+  new Ingredient("water", 200, "ml"),
+  new Ingredient("milk", 50, "ml"),
+  new Ingredient("coffee beans", 15, "g")
+];
+
+let coffeeMachine = {
+  readSuppliesAmount() {
+    coffee.forEach((ingredient) => {
+      let prompt= `Write how many ${ingredient.unit}`;
+      prompt += ` of ${ingredient.name} the coffee machine has:\n`;
+      this[ingredient.name] = input(prompt);
+    })
+    // console.log(`Available cups: ${this.getAvailableCups()}`);
+  },
+
+  getAvailableCups() {
+    let availableCups = Infinity;
+    coffee.forEach((ingredient) => {
+      availableCups = Math.min(availableCups
+        , Math.floor(this[ingredient.name] / ingredient.amount));
+    });
+    return availableCups;
+  },
+
+  readOrder() {
+    const prompt = "Write how many cups of coffee you will need:\n";
+    this.orderedCups = input(prompt);
+  },
+
+  processOrder() {
+    const diff = this.getAvailableCups() - this.orderedCups;
+    const enoughSuppliesMsg
+      = "Yes, I can make that amount of coffee";
+    const notEnoughSuppliesMsg
+        = `No, I can make only ${this.getAvailableCups()} cups of coffee`;
+    const extraSuppliesMsg
+        = ` (and even ${diff} more than that)`;
+    let response = "";
+    if (diff < 0) {
+      response = notEnoughSuppliesMsg;
+    } else {
+      response = enoughSuppliesMsg;
+      if (diff > 0) response += extraSuppliesMsg;
+    }
+    console.log(response);
+  },
+
+  init() {
+    this.readSuppliesAmount();
+    this.readOrder();
+    this.processOrder();
+  }
+};
+
+coffeeMachine.init();
