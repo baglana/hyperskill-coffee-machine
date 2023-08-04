fa7f9b4e4f6f880c6dfd18071857010ced233735
baglana <102351104+baglana@users.noreply.github.com>
2023-08-05 00:39:03 +0600

    4/6: Buy, fill, take!

--- main.js
+++ main.js
@@ -15,65 +15,115 @@ Coffee is ready!`
 );
 */
 
+/*
 function Ingredient(name, amount, unit) {
   this.name = name;
   this.amount = amount;
   this.unit = unit;
 }
+*/
 
-const coffee = [
-  new Ingredient("water", 200, "ml"),
-  new Ingredient("milk", 50, "ml"),
-  new Ingredient("coffee beans", 15, "g")
+function Coffee(id, name, water, milk, coffeeBeans, price) {
+  this.id = id;
+  this.name = name;
+  this.water = water;
+  this.milk = milk
+  this.coffeeBeans = coffeeBeans;
+  this.price = price;
+}
+
+const coffeeFlavors = [
+  new Coffee(1, "espresso", 250, 0, 16, 4),
+  new Coffee(2, "latte", 350, 75, 20, 7),
+  new Coffee(3, "cappuccino", 200, 100, 12, 6)
 ];
 
-let coffeeMachine = {
-  readSuppliesAmount() {
-    coffee.forEach((ingredient) => {
-      let prompt= `Write how many ${ingredient.unit}`;
-      prompt += ` of ${ingredient.name} the coffee machine has:\n`;
-      this[ingredient.name] = input(prompt);
+function CoffeeMachine(water, milk, coffeeBeans, dispCups, money) {
+  this.water = water;
+  this.milk = milk;
+  this.coffeeBeans = coffeeBeans;
+  this.dispCups = dispCups;
+  this.money = money;
+  
+  this.printState = () => {
+    console.log("The coffee machine has:");
+    console.log(`${this.water} ml of water`);
+    console.log(`${this.milk} ml of milk`);
+    console.log(`${this.coffeeBeans} g of coffee beans`);
+    console.log(`${this.dispCups} disposable cups`);
+    console.log(`$${this.money} of money\n`);
+  };
+  
+  this.replenishSupplies = () => {
+    this.water += Number(input(
+      "Write how many ml of water you want to add:\n"));
+    this.milk += Number(input(
+      "Write how many ml of milk you want to add:\n"));
+    this.coffeeBeans += Number(input(
+      "Write how many grams of coffee beans you want to add:\n"));
+    this.dispCups += Number(input(
+      "Write how many disposable cups you want to add:\n"));
+  };
+
+  this.askForChoice = () => {
+    let prompt = "What do you want to buy? ";
+    coffeeFlavors.forEach((flavor) => {
+      prompt += `${flavor.id} - ${flavor.name}, `;
     })
-    // console.log(`Available cups: ${this.getAvailableCups()}`);
-  },
-
-  getAvailableCups() {
-    let availableCups = Infinity;
-    coffee.forEach((ingredient) => {
-      availableCups = Math.min(availableCups
-        , Math.floor(this[ingredient.name] / ingredient.amount));
-    });
-    return availableCups;
-  },
-
-  readOrder() {
-    const prompt = "Write how many cups of coffee you will need:\n";
-    this.orderedCups = input(prompt);
-  },
-
-  processOrder() {
-    const diff = this.getAvailableCups() - this.orderedCups;
-    const enoughSuppliesMsg
-      = "Yes, I can make that amount of coffee";
-    const notEnoughSuppliesMsg
-        = `No, I can make only ${this.getAvailableCups()} cups of coffee`;
-    const extraSuppliesMsg
-        = ` (and even ${diff} more than that)`;
-    let response = "";
-    if (diff < 0) {
-      response = notEnoughSuppliesMsg;
-    } else {
-      response = enoughSuppliesMsg;
-      if (diff > 0) response += extraSuppliesMsg;
-    }
-    console.log(response);
-  },
-
-  init() {
-    this.readSuppliesAmount();
-    this.readOrder();
-    this.processOrder();
+    prompt = prompt.slice(0, -2);
+    prompt += ":\n";
+    return input(prompt);
   }
-};
+  
+  this.processOrder = (id) => {
+    const flavor = coffeeFlavors.find(f => f.id === id);
+    this.water -= flavor.water;
+    this.milk -= flavor.milk;
+    this.coffeeBeans -= flavor.coffeeBeans;
+    this.dispCups--;
+    this.money += flavor.price;
+  }
+  
+  this.sell = () => {
+    let choice = Number(this.askForChoice());
+    this.processOrder(choice);
+  }
 
-coffeeMachine.init();
+  this.giveMoney = () => {
+    const givenMoney = this.money;
+    this.money -= givenMoney;
+    console.log(`I gave you $${givenMoney}\n`);
+  }
+
+  this.promptUser = () => {
+    const prompt = "Write action (buy, fill, take):\n";
+    const choice = input(prompt);
+
+    switch (choice) {
+      case "buy":
+        this.sell();
+        break;
+      case "fill":
+        this.replenishSupplies();
+        break;
+      case "take":
+        this.giveMoney();
+        break;
+      default:
+        break;
+    }
+    
+    console.log();
+    this.printState();
+  }
+
+  this.start = () => {
+    this.printState();
+    this.promptUser();
+  }
+  
+}
+
+const coffeeMachine
+  = new CoffeeMachine(400, 540, 120, 9, 550);
+coffeeMachine.start();
